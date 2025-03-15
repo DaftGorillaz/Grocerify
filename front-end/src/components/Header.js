311f4d2bb9e9780182554902914e99755a788f50
Simone Bellandi <lebomiss@protonmail.com>
2025-03-15 23:02:44 +1100

    Implement user context for user handling

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Header.css';
 import locationIcon from '../assets/images/location-icon.svg';
+import { useUser } from '../context/UserContext';
 
 const Header = () => {
+  const { user } = useUser();
+  
   return (
     <div className="header">
-      <h1 className="greeting">Hello, Steve!</h1>
+      <h1 className="greeting">Hello, {user.name}!</h1>
       <div className="location">
         <img src={locationIcon} alt="Location" className="location-icon" />
-        <p>Melbourne, Victoria</p>
+        <p>{user.location}</p>
       </div>
     </div>
   );
